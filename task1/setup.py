58563566d929f0961525bb230641fafe8f343da6
Vishwas Magar <113243068+vishwasmagar10@users.noreply.github.com>
2024-05-14 14:32:59 +0530

    Add files via upload

--- task1/setup.py
+++ task1/setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup
 
-with open("README.md","r",encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-AUTHOR_NAME = 'VISHWAS'
-SRC_REPO = 'src'
+## edit below variables as per your requirements -
+REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
+AUTHOR_USER_NAME = "vishwas"
+SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit']
 
+
 setup(
-    name = SRC_REPO,
-    version= '0.0.1',
-    author = AUTHOR_NAME,
-    author_email= 'vishwasmagar10@gmail.com',
-    description = 'A small example package for movie recommendation',
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
     long_description=long_description,
-    long_description_content_type= 'text/markdown',
-    packages = [SRC_REPO],
-    python_requires = '>=3.7',
-    install_requires = LIST_OF_REQUIREMENTS,
+    long_description_content_type="text/markdown",
+    author_email="vishwasmagar10@gmail.com",
+    packages=[SRC_REPO],
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
 )
